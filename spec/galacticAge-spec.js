d35438bf5384fa95d6f372b805c632096ab9f186
Karla Buckner <kmb2662@gmail.com>
2017-10-16 22:06:35 -0700

    add adjustments to specs

--- spec/galacticAge-spec.js
+++ spec/galacticAge-spec.js
@@ -4,50 +4,52 @@
 
 import { GalacticAge } from './../js/galacticAge.js';
 
-describe('GalacticAge', () {
-  let test_galacticAge;
+describe('GalacticAge', function () {
+  let myAge;
+  let surpassExp;
 
-  beforeEach(() {
-    test_galacticAge = new GalacticAge("04/05/1990");
+  beforeEach(function() {
+    myAge = new GalacticAge(27,"04/05/1990");
+    surpassExp = new GalacticAge(85,"07/07/1932");
   });
 
   it('takes DOB input from user', function(){
-    expect(test_galacticAge.birthdate).toEqual('04/05/1990')
+    expect(myAge.birthdate)).toEqual('04/05/1990')
   });
 
-  it('determines age by the difference between current time and entered DOB', function(){
-    expect(test_galacticAge.ageInYears()).toEqual(27)
+  it('determines age by the difference between current time and entered DOB', function() {
+    expect(myAge.ageInYears()).toEqual(27)
   });
 
   it('takes user input of age in years on Earth and converts to seconds', function() {
-    expect(test_galacticAge.ageInSeconds()).toEqual(851472000);
+    expect(myAge.ageInSeconds()).toEqual(851472000);
   });
 
   it('converts years on Earth to years on Mercury', function() {
-    expect(test_galacticAge.ageOnMercury()).toEqual(6);
+    expect(myAge.ageOnMercury()).toEqual(6);
   });
 
   it('converts years on Earth to years on Venus', function() {
-    expect(test_galacticAge.ageOnVenus()).toEqual(17);
+    expect(myAge.ageOnVenus()).toEqual(17);
   });
 
   it('converts years on Earth to years on Mars', function() {
-    expect(test_galacticAge.ageOnMars()).toEqual(51);
+    expect(myAge.ageOnMars()).toEqual(51);
   });
 
   it('converts years on Earth to years on Jupiter', function() {
-    expect(test_galacticAge.ageOnJupiter()).toEqual(320);
+    expect(myAge.ageOnJupiter()).toEqual(320);
   });
 
   it('calculates life expectancy', function() {
-    expect(test_galacticAge.lifeExpectancy("female", "northAmerica", "yes", "no")).toEqual(85);
+    expect(myAge.lifeExpectancy("female", "northAmerica", "yes", "no")).toEqual(85);
   });
 
   it('tells user how much time left to live', function() {
-    expect(test_galacticAge.yearsLeftEarth(27, 85)).toEqual(58);
+    expect(myAge.yearsLeftEarth(27, 85)).toEqual(58);
   });
 
   it('tells user how far past life expectancy lived', function() {
-      expect(test_galacticAge.overLifeExpectancy(85, 75)).toEqual(10)
+      expect(surpassExp.overLifeExpectancy(85, 75)).toEqual(10);
   });
 });
